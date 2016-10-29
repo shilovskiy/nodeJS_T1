57fe416ad250fffefc8562153d787efc9b1c0606
shilovskiy <shilovskiy@gmail.com>
2016-10-29 14:48:21 +0300

    task1

--- index.js
+++ index.js
@@ -3,3 +3,20 @@ var tt=2;
 ss='Lorem ipsum dolor sit amet, consectetur adipisicing elit';
 
 console.log(ss);
+
+
+class Pokemon {
+  constructor(name,level) {
+    this.name = name;
+    this.level = level;
+  }
+  sayHello(){
+    console.log( "hi!, i'm %s", this.name );
+  }
+  valueof(){
+    return this;
+  }
+}
+let pok1 = new Pokemon("Vasj1a",5);
+pok1.sayHello();
+console.log(pok1.valueof());
