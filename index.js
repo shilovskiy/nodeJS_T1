bce7aca2e44c40a841606b634e13d3cc69c73705
shilovskiy <shilovskiy@gmail.com>
2016-10-29 11:50:24 +0300

    ok
    d

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 console.log('Hello world');
 var tt=2;
-ss='Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+ss='Lorem ipsum dolor sit amet, consectetur adipisicing elit';
 
 console.log(ss);
-//x3rc34
