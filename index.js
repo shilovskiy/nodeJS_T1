efb32f8314da48db159fd786e49002432fdc7d0d
shilovskiy <shilovskiy@gmail.com>
2016-10-30 12:01:05 +0300

    new version

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 console.log('Week 1. Task 1-2');
-var tt=2;
-ss='Lorem ipsum dolor sit amet, consectetur adipisicing elit';
-
-console.log(ss);
 
 //Часть 1:
 //— Создать класс Pokemon , конструктор которого принимает имя и
@@ -14,7 +10,7 @@ class Pokemon {
     this.level = level;
   }
   show(){
-    console.log( "hi!, i'm %s", this.name );
+      console.log("Pokemon %s level %d",this.name,this.level);
   }
   valueof(){
     return this;
@@ -32,17 +28,85 @@ class PokemonList extends Array {
   last() {
       return this[this.length - 1];
   }
+  add(name,level){
+    this.push(new Pokemon(name,level));
+  }
+  //общее количество и список
+  show(){
+    for(let pok of this){
+        pok.show();
+      //console.log("Pokemon %s level %d",pok.name,pok.level);
+    }
+    console.log("Total amount: %d",this.length);
+
+  }
+
+  getPokemon(){
+    return this.pop();
+  }
+  max(){
+    //return  Math.max.apply(null, this.map(item => item.level));
+   return this.reduce((prev,curr)=>{
+     console.log(prev);
+     console.log(curr);
+     prev.level<curr.Level ? prev : curr} )
+  }
 }
 
 
 
 let pok1 = new Pokemon("Vasj1a",5);
-pok1.sayHello();
+pok1.show();
 console.log(pok1.valueof());
+
 
 let pok2 = new Pokemon("Pikachu",80);
 
 
 
 let poklist = new PokemonList(pok1,pok2);
-console.log(poklist.last());
+console.log(poklist);
+poklist.add("testman",85);
+console.log(poklist);
+
+
+let lost = new PokemonList();
+lost.add("Crotine",12);
+lost.add("Barrados",17);
+lost.add("Turteler",22);
+lost.add("Hippopoom",27);
+lost.add("Stunhopper",32);
+lost.add("Hypepie",37);
+lost.add("Bellibia",42);
+lost.add("Venomeleon",47);
+lost.add("Magmeleon",52);
+lost.add("Dinoscythe",57);
+console.log(lost);
+
+
+
+let found = new PokemonList();
+
+console.log(lost);
+
+found.add("Catectric",15);
+found.add("Wallabite",19);
+found.add("Wolvia",24);
+found.add("Jagoss",29);
+found.add("Eleron",34);
+found.add("Whirlphant",39);
+found.add("Glacamel",44);
+found.add("Fluffanzee",49);
+found.add("Repelican",54);
+found.add("Ursign",59);
+
+//console.log(found);
+found.show();
+
+
+let new_pok=lost.getPokemon();
+console.log(new_pok);
+
+found.add(new_pok.name,new_pok.level);
+found.show();
+console.log('max',found.max());
